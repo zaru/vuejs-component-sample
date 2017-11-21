30afd3e2dcf3ebd50c4286b0df3e1c3da6543877
zaru <sakuraba@basicinc.jp>
2017-11-21 10:24:50 +0900

    🚕

---
+++ src/components/Child.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="child">
+    <h2>Child component</h2>
+    <p>From parent: {{ parentToChild }}</p>
+    <p>From parent: {{ dynamicParentToChild }}</p>
+    <p>Sync parent: {{ syncParentChild }}</p>
+    <ul>
+      <li><button type="button" v-on:click="childCountUp">count up</button></li>
+      <li><button type="button" v-on:click="childSyncCountUp">sync count up</button></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Child',
+  props: [
+    'parentToChild',
+    'dynamicParentToChild',
+    'syncParentChild'
+  ],
+  data () {
+    return {
+    }
+  },
+  methods: {
+    childCountUp: function (e) {
+      this.$emit('child-to-parent')
+    },
+    childSyncCountUp: function (e) {
+      var newCount = this.syncParentChild + 1
+      this.$emit('update:syncParentChild', newCount)
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/Parent.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="parent">
+    <h1>Parent</h1>
+    <p>Counter {{ parentCounter }}</p>
+    <p>Sync Counter {{ syncCounter }}</p>
+    <p><button type="button" v-on:click="countUp">count up</button></p>
+    <p><button type="button" v-on:click="syncCountUp">sync count up</button></p>
+    <child
+      parent-to-child="Hello child component"
+      v-bind:dynamic-parent-to-child="parentCounter"
+      v-on:child-to-parent="countUp"
+      v-bind:sync-parent-child.sync="syncCounter"
+      ></child>
+  </div>
+</template>
+
+<script>
+import Child from '@/components/Child'
+
+export default {
+  name: 'Parent',
+  components: {
+    Child
+  },
+  data () {
+    return {
+      parentCounter: 0,
+      syncCounter: 0
+    }
+  },
+  methods: {
+    countUp: function (e) {
+      this.parentCounter += 1
+    },
+    syncCountUp: function (e) {
+      this.syncCounter += 1
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
