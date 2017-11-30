f6ed5e2e3f2297da918ad8e268157610a417c324
zaru <sakuraba@basicinc.jp>
2017-11-30 09:36:20 +0900

    🚖

--- src/components/modal/ModalSimple.vue
+++ src/components/modal/ModalSimple.vue
@@ -1,23 +1,23 @@
 <template>
   <div>
     <modal
-      ref="modal"
-      @beforeOpen="beforeOpen"
-      @afterOpen="afterOpen"
-      @beforeClose="beforeClose"
-      @afterClose="afterClose"
+      ref='modal'
+      @beforeOpen='beforeOpen'
+      @afterOpen='afterOpen'
+      @beforeClose='beforeClose'
+      @afterClose='afterClose'
     >
       <p>main content</p>
 
-      <div slot="header">
+      <div slot='header'>
         <h3>modal header</h3>
       </div>
 
-      <div slot="footer">
-        <button @click="close">close</button>
+      <div slot='footer'>
+        <button @click='close'>close</button>
       </div>
     </modal>
-    <button @click="open">open</button>
+    <button @click='open'>open</button>
   </div>
 </template>
 
@@ -33,7 +33,38 @@ export default {
       modalActive: true
     }
   },
+  beforeCreate: function () {
+    console.log('beforeCreate')
+    console.log(this.$refs)
+  },
+  created: function () {
+    console.log('created')
+    console.log(this.$refs)
+  },
+  beforeMount: function () {
+    console.log('beforeMount')
+    console.log(this.$refs)
+  },
   mounted: function () {
+    // ここで初めて refs が参照できることに注意
+    console.log('mounted')
+    console.log(this.$refs)
+  },
+  beforeUpdate: function () {
+    console.log('beforeUpdate')
+    console.log(this.$refs)
+  },
+  updated: function () {
+    console.log('updated')
+    console.log(this.$refs)
+  },
+  beforeDestroy: function () {
+    console.log('beforeDestroy')
+    console.log(this.$refs)
+  },
+  destroyed: function () {
+    console.log('destroyed')
+    console.log(this.$refs)
   },
   methods: {
     beforeOpen: function (e) {
